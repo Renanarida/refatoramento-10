c0e56aaae78b7853f7c2cb44a6d877077c60a112
Renanarida <arida.renan100@gmail.com>
2025-09-18 16:45:32 -0300

    teste

---
+++ backend/resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>App</title>
+  @viteReactRefresh
+  @vite(['resources/js/main.jsx', 'resources/css/app.css'])
+</head>
+<body class="antialiased">
+  <div id="root"></div>
+</body>
+</html>
